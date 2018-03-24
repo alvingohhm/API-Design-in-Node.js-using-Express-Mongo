e33725cde1d3dc177c61f5e029df0c3b6c96d2d8
Kan1shka9 <kanishka.s9@gmail.com>
2018-03-23 19:43:07 -0700

    promises

--- promises-example/index.js
+++ promises-example/index.js
@@ -1,12 +1,27 @@
-var action = function() {
-	return new Promise(function(resolve, reject) {
-		setTimeout(function() {
-			resolve('hey');
-		},5000);
-	})
+var fs = require('fs');
+
+var readFile = function() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile('./package.json', function(err, file) {
+      return err ? reject(err) : resolve(file.toString());
+    });
+  });
 };
 
-action()
-	.then(function(word){
-		console.log(word);
-	});
+var readAllFiles = function() {
+  var promises = [readFile(), readFile(), readFile()];
+  return Promise.all(promises);
+};
+
+
+var logFile = function() {
+  return readFile()
+    .then(function() {
+      return readFile()
+    });
+}
+// start a loading
+readAllFiles()
+  .then(function(files) {
+    console.log(files.length);
+  });
